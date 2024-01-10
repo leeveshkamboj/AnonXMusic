d608a58b03db9b45422de5ea4b8adee533e34e05
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2024-01-10 21:45:15 +0530

    Update config.py
    Signed-off-by: Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_NAME = getenv("MONGO_DB_NAME", "Anon")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
