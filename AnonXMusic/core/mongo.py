a64d04c9a8767e8e642fd32b961a76878003870f
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2024-01-10 21:45:37 +0530

    Update mongo.py
    Signed-off-by: Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>

--- AnonXMusic/core/mongo.py
+++ AnonXMusic/core/mongo.py
@@ -1,13 +1,13 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from config import MONGO_DB_URI
+from config import MONGO_DB_URI, MONGO_DB_NAME
 
 from ..logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
+    mongodb = _mongo_async_[MONGO_DB_NAME]
     LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
     LOGGER(__name__).error("Failed to connect to your Mongo Database.")
